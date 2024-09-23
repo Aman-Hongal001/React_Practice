473c3aedfa452ce8fc4f2a51dd276f3ade0a1a35
Aman <amanhongal@gmail.com>
2024-09-23 18:03:58 +0530

    Tailwind setup with react framework is completed successfully

--- secondapp/src/App.js
+++ secondapp/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './images/img.png'
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* images from direct web */}
+      <p className='text-left'>Images used from local storage</p>
+      <img width={100} src={logo} alt="Img from local storage" />
+      <p className='text-left'>Images used from direct url from web</p>
+      <img width={100} src='https://i.pinimg.com/736x/da/c7/58/dac758d7606690d057a9cbba5f259154.jpg' alt="Img from web" />
+      <Header/>
+      <h1 className='text-[40px] text-red-700 font-bold'>Welcome to the Second app</h1>
     </div>
   );
 }
