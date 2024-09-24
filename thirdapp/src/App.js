0861879b1ad67cbbf8a90c66f9812b3f621e993b
Aman <amanhongal@gmail.com>
2024-09-24 08:28:37 +0530

    Responsive task or nav bar is created with state management and ternary operator

--- thirdapp/src/App.js
+++ thirdapp/src/App.js
@@ -6,9 +6,25 @@ import { useState } from 'react';
 function App() {
 
   let [pstatus,setPstatus]=useState(false)
+  let [menuStatus,setMenuStatus]=useState(false)
 
   return (
     <div className="App">
+
+      <button className='micon' onClick={()=>setMenuStatus(!menuStatus)}>
+        {menuStatus ? <span>&times;</span>:<span>&#9776;</span>}
+      </button>
+      <div className={`menu ${menuStatus ? 'activeMenu':''}`} >
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Resume</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+
       <input type={(pstatus ? 'text':'password')} placeholder='Enter password'/>
       <button onClick={()=>setPstatus(!pstatus)}>{(pstatus ? 'Hide':'Show')}</button>
     </div>
