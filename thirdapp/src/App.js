6baf7e8f96b54055226ce6136c5237a5f5961374
Aman <amanhongal@gmail.com>
2024-09-24 12:44:22 +0530

    data rendering and showing throw state management is studied

--- thirdapp/src/App.js
+++ thirdapp/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { faqQuestions } from './data/faq';
 
 
 function App() {
@@ -8,18 +9,20 @@ function App() {
   let [pstatus,setPstatus]=useState(false)
   let [menuStatus,setMenuStatus]=useState(false)
   let [modalStatus,setmodalStatus]=useState(false)
+  let [showAns,setshowAns]=useState(faqQuestions[0].id)
 
   return (
     <div className="App">
 
+      {/* side scroll down modal  */}
       <button className='en' onClick={()=>setmodalStatus(true)}>Explore Know</button>
-
       <div className={`modalOverlay ${modalStatus ? 'showmodalOverlay':''} `} onClick={()=>setmodalStatus(false)}></div>
       <div className={`modelDiv ${modalStatus ? 'showmodelDiv':''} `}>
         <h3>Enquiry Know</h3>
       </div>
 
 
+      {/* nav bar creation */}
       <button className='micon' onClick={()=>setMenuStatus(!menuStatus)}>
         {menuStatus ? <span>&times;</span>:<span>&#9776;</span>}
       </button>
@@ -34,8 +37,26 @@ function App() {
       </div>
 
 
+      {/* password button and input  */}
       <input type={(pstatus ? 'text':'password')} placeholder='Enter password'/>
       <button onClick={()=>setPstatus(!pstatus)}>{(pstatus ? 'Hide':'Show')}</button>
+
+
+
+      <div className='faqDiv'>
+        <h1>Frequently Asked Questions Sample Using State Management</h1>
+        {faqQuestions.map((faqitems,i)=>{
+        return(
+          <div className='faqitems'>
+            <h2 className='faqQuestion' onClick={()=>setshowAns(faqitems.id)}>{faqitems.question}</h2>
+            <p className={showAns===faqitems.id ? 'faqAnsShow':''}>{faqitems.answere}</p>
+          </div>  
+          );
+        })}
+      </div>
+
+
+
     </div>
   );
 }
