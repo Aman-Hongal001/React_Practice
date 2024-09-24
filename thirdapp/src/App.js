b0e65bea1618f8ad955a61642a02d8c6018d4933
Aman <amanhongal@gmail.com>
2024-09-24 07:28:30 +0530

    small task to create password hide and show using state management is completed

---
+++ thirdapp/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+
+  let [pstatus,setPstatus]=useState(false)
+
+  return (
+    <div className="App">
+      <input type={(pstatus ? 'text':'password')} placeholder='Enter password'/>
+      <button onClick={()=>setPstatus(!pstatus)}>{(pstatus ? 'Hide':'Show')}</button>
+    </div>
+  );
+}
+
+export default App;
