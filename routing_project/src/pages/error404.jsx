66d685d460f66bb2d45ce2774d5a0abde286c1dc
Aman <amanhongal@gmail.com>
2024-09-25 19:33:15 +0530

    The Earlier commit message

---
+++ routing_project/src/pages/error404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../component/header";
+
+export default function Error404() {
+  return( 
+  <div>
+    <Header/>
+    <h1>Error404 Page Not Found</h1>
+  </div>
+
+  );
+}
